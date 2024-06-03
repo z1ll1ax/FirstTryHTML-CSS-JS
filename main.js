fc562d4b11316f8eacb126e493560e748a950d14
z1ll1ax <vlad_semenenko_2016@mail.ru>
2024-06-03 23:51:42 +0300

    added-some-new-tabs

---
+++ main.js
@@ -0,0 +1,12 @@
+function MyCart(){
+    window.location.href = "my-cart.html";
+}
+function Catalog(){
+    window.location.href = "catalog.html";
+}
+function AboutUs(){
+    window.location.href = "about-us.html";
+}
+function Contacts(){
+    window.location.href = "contacts.html";
+}
